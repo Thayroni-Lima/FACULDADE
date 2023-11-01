3718f21ad8cad4e1e5c7ecd361d61f89a4e3cb26
ThayroniLima <130916851+Thayroni-Lima@users.noreply.github.com>
2023-11-01 15:42:19 -0300

    php

---
+++ PHP/atividade8/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Média de alunos</title>
+</head>
+<body>
+    <!-- Faça uma aplicação Web que permita ao usuário
+    - digitar o nome de um aluno
+    - suas duas notas na disciplina de Programação Web
+    -  Em seguida faça um script PHP que receba 
+        - os dados submetidos a partir do formulário
+        - calcula a média do aluno
+        - exiba a seguinte mensagem: “O aluno XXXXX ficou com XX de média”. -->
+        
+        <form action="" method="post">
+            <label for="">Thayroni  </label>
+        </form>
+</body>
+</html>
